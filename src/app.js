d496051593efd572ad2b7d3aa1aec3274687188d
KevinC-hub <canchonkevin@gmail.com>
2025-03-01 01:51:17 +0000

    Need to finish

--- src/app.js
+++ src/app.js
@@ -6,6 +6,20 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  generaterandomcard()
 };
+const generaterandomcard = () => {
+  let icon = ["♦", "♥", "♠", "♣"]
+  let number = ["A","K","Q","J","10","9","8","7","6","5","4","3","2"];
+  
+  let iconIndex = Math.floor(Math.random() * icon.length)
+  let numberIndex = Math.floor(Math.random() * number.length)
+
+  let randomIcon = icon[iconIndex]
+  let randomNumber = number[numberIndex]
+
+
+  document.querySelector(".top").innerHTML = randomIcon
+  document.querySelector(".number").innerHTML = randomNumber
+  document.querySelector(".bottom").innerHTML = randomIcon
+}
