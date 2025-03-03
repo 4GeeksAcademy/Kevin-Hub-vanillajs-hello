275c259c1aaea13b138624a2a31fa3f9e826a009
KevinC-hub <canchonkevin@gmail.com>
2025-03-03 05:31:00 +0000

    Done

--- src/app.js
+++ src/app.js
@@ -5,10 +5,17 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+// function
 window.onload = function() {
   generaterandomcard()
+  document.querySelector("#randomcardBtn").addEventListener("click", generaterandomcard)
 };
+
+// random card generator
 const generaterandomcard = () => {
+  const iconDiv = document.querySelector(".bottom",)
+  const icondiv = document.querySelector(".top",)
+  
   let icon = ["♦", "♥", "♠", "♣"]
   let number = ["A","K","Q","J","10","9","8","7","6","5","4","3","2"];
   
@@ -18,8 +25,36 @@ const generaterandomcard = () => {
   let randomIcon = icon[iconIndex]
   let randomNumber = number[numberIndex]
 
+  // code for bottom icon
+  if(randomIcon == "♦") {
+    iconDiv.style.color = "red"
+  }
+  else if(randomIcon == "♥") {
+    iconDiv.style.color = "red"
+  }
+  else if(randomIcon == "♣") {
+    iconDiv.style.color = "black"
+  }
+  else if(randomIcon == "♠") {
+    iconDiv.style.color = "black"
+  }
+// Code for top icon
+  if(randomIcon == "♦") {
+    icondiv.style.color = "red"
+  }
+  else if(randomIcon == "♥") {
+    icondiv.style.color = "red"
+  }
+  else if(randomIcon == "♣") {
+    icondiv.style.color = "black"
+  }
+  else if(randomIcon == "♠") {
+    icondiv.style.color = "black"
+  }
+
 
   document.querySelector(".top").innerHTML = randomIcon
   document.querySelector(".number").innerHTML = randomNumber
   document.querySelector(".bottom").innerHTML = randomIcon
+  
 }
